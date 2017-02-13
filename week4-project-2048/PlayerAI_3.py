18f4518b53a9ae068f7b7ca2266f546b1af7e7e8
ChadoNihi <chadoishere@gmail.com>
2017-02-14 01:13:20 +0200

    save

--- week4-project-2048/PlayerAI_3.py
+++ week4-project-2048/PlayerAI_3.py
@@ -1,7 +1,17 @@
+from operator import itemgetter
+
 from random import randint
 from BaseAI import BaseAI_3
 
 class PlayerAI(BaseAI):
     def getMove(self, grid):
         moves = grid.getAvailableMoves()
-        return moves[randint(0, len(moves) - 1)] if moves else None
+        return max([(i, v) for i, v in moves], key=itemgetter(1))
+
+    def minimax(self, grid, a, b, depth, isMaxPlayer):
+        if timeout or tooDeep:
+            return h(grid)
+        elif isMaxPlayer:
+            pass
+        else:
+            pass
