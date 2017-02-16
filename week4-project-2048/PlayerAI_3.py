cea66c83b6ed3fee76368f62c7917b9c3e244fa0
ChadoNihi <chadoishere@gmail.com>
2017-02-16 14:15:58 +0200

    save

--- week4-project-2048/PlayerAI_3.py
+++ week4-project-2048/PlayerAI_3.py
@@ -1,31 +1,51 @@
 from math import inf
 from operator import itemgetter
+from random import random
 
 from random import randint
 from BaseAI import BaseAI_3
 
 class PlayerAI(BaseAI):
-    PROB_2 = 1
-    PROB_4 = 1-self.PROB_2
+    PROB_4 = 0
     TILL_DEPTH = 5
 
     def getMove(self, grid):
         moves = grid.getAvailableMoves()
-        vs = []
+        v = -inf
+        bestDir = None
         for dir in moves:
-            movedGrid = grid.clone()
-            movedGrid.move(dir)
-            vs.append(self.minimax(movedGrid, -inf, inf,  1))
-        return (max([(i, v) for i, v in enumerate(vs)], key=itemgetter(1)))[0]
+            nextGrid = grid.clone()
+            nextGrid.move(dir)
+            vCand = max(v, minimax(nextGrid, a, b, incredDepth, False))
+
+            if vCand > v:
+                v = vCand
+                bestDir = dir
+        return bestDir
 
     def minimax(self, grid, a, b, depth, isMaxPlayer=False):
         if timeout or depth == self.TILL_DEPTH:
             return h(grid)
         elif isMaxPlayer:
-            pass
+            incredDepth = depth + 1
+            v = -inf
+            moves = grid.getAvailableMoves()
+            for dir in moves:
+                nextGrid = grid.clone()
+                nextGrid.move(dir)
+                v = max(v, minimax(nextGrid, a, b, incredDepth, False))
+
+                a = max(a, v)
+                if b <= a: break
         else:
-            cells = grid.getAvailableCells()
+            incredDepth = depth + 1
             v = inf
-            for cell in cells:
-                pass
-            return list(raise StopIteration if b<=a else )
+            emptyCells = grid.getAvailableCells() #what order promotes early pruning?
+            for cell in emptyCells:
+                nextGrid = grid.clone()
+                nextGrid.insertTile(cell, 2 if random() < self.PROB_4 else 4)
+                v = min(v, minimax(nextGrid, a, b, incredDepth, True))
+
+                b = min(b, v)
+                if b <= a: break
+            return v
